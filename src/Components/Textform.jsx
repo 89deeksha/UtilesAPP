d2995e8bc844c2a8c10a42f52ceff3e4649c6801
Deeksha Jain <deekshajain0310@gmail.com>
2025-01-15 22:23:11 +0530

    ready to use

--- src/Components/Textform.jsx
+++ src/Components/Textform.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
+
 function Textform(props) {
+  const [text, setText] = useState("enter text here");
+  const handleUpclick = () => {
+    console.log("Uppercaseclicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleonchange = (e) => {
+    console.log("onchangeclicked");
+    setText(e.target.value);
+  };
   return (
     <div>
       <h1>{props.heading}</h1>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={8} />
+          <Form.Control
+            as="textarea"
+            value={text}
+            onChange={handleonchange}
+            rows={8}
+          />
         </Form.Group>
       </Form>
-      <button className="btn btn-primary">Convert to uppercase</button>
+      <button className="btn btn-primary" onClick={handleUpclick}>
+        Convert to uppercase
+      </button>
     </div>
   );
 }
