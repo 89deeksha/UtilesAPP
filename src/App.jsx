f7250272ffa436582b750aca619920093bc7e551
Deeksha Jain <deekshajain0310@gmail.com>
2025-01-15 17:56:07 +0530

    utiles

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import NavScrollExample from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <NavScrollExample />
+    </>
+  );
+}
+
+export default App;
